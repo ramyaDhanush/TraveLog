89f3c2e6d1a8b565cd29a9d1ac1330543604af6c
Ramya D <ramyadhanush99@gmail.com>
2021-02-15 22:24:40 +0530

    Feat : Server Basic middlewares
    Signed-off-by: Ramya D <ramyadhanush99@gmail.com>

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ A full (MERN) stack application for creating & listing travel logs
 
 # Contents
 
-[✔] Setup Server
+[✔] Setup Server  
   [✔] Install Dependencies
   [✔] Install/Setup Linter
   [✔] Setup Express App
-  [❕] Setup Not Found & Error Middlewares
+  [✔] Setup Not Found & Error Middlewares
 [❕] Model DB
   [❕] What data will we store?
 [❕] Setup Mongoose Model(s)
--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,27 @@ app.use(cors({
 
 const port = process.env.PORT || 1337; 
 
+app.get('/', (req, res) => {
+  res.json({
+    message:'Hello World!',
+  });
+})
+
+app.use((req, res, next) => { //last middleware
+  const err = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(err);
+});
+
+app.use((error, req, res, next) => { // error handler should have 4 arguments with error as fist one
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? ' A bug in URL ' : error.stack
+  })
+})
+
 app.listen(port, () => {  
   console.log(`Listening at http://localhost:${port}`);
 });
